0055b1ffc2c045dfa13e2e53f20271616c6639df
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2021-11-02 12:45:39 -0400

    Create searchMatrix.js

---
+++ searchMatrix.js
@@ -0,0 +1,66 @@
+
+// 74. Search a 2D Matrix
+// Medium
+
+// 4911
+
+// 224
+
+// Add to List
+
+// Share
+// Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+// Integers in each row are sorted from left to right.
+// The first integer of each row is greater than the last integer of the previous row.
+ 
+
+// Example 1:
+
+
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+// Output: true
+// Example 2:
+
+
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+// Output: false
+ 
+
+// Constraints:
+
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= m, n <= 100
+// -104 <= matrix[i][j], target <= 104
+
+
+
+
+
+
+
+
+
+// Success
+// Details 
+// Runtime: 76 ms, faster than 72.64% of JavaScript online submissions for Search a 2D Matrix.
+// Memory Usage: 39 MB, less than 59.88% of JavaScript online submissions for Search a 2D Matrix.
+
+
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    
+    for (let i =0; i<matrix.length;i++){
+        let col = matrix[i]
+        
+        if(col.includes(target)){return true}
+    }
+    
+    return false
+};
