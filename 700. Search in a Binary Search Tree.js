7e16c7c76128a748c83106a5aff6d19552b3d5d4
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2021-11-10 07:32:06 -0500

    Create 700. Search in a Binary Search Tree.js

---
+++ 700. Search in a Binary Search Tree.js
@@ -0,0 +1,53 @@
+// You are given the root of a binary search tree (BST) and an integer val.
+
+// Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [4,2,7,1,3], val = 2
+// Output: [2,1,3]
+// Example 2:
+
+
+// Input: root = [4,2,7,1,3], val = 5
+// Output: []
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 5000].
+// 1 <= Node.val <= 107
+// root is a binary search tree.
+// 1 <= val <= 107
+
+
+Success
+Details 
+Runtime: 88 ms, faster than 89.19% of JavaScript online submissions for Search in a Binary Search Tree.
+Memory Usage: 45.4 MB, less than 71.67% of JavaScript online submissions for Search in a Binary Search Tree.
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    if(root == null || root.val == val){
+        return root
+    }
+    
+    return root.val > val ? searchBST(root.left,val):searchBST(root.right,val)
+};
