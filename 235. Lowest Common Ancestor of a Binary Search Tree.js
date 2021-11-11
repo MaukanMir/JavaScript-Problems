e582ca0e6620dc7d8ccf73ae84828da0f715877a
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2021-11-11 13:13:22 -0500

    Update 235. Lowest Common Ancestor of a Binary Search Tree.js

--- 235. Lowest Common Ancestor of a Binary Search Tree.js
+++ 235. Lowest Common Ancestor of a Binary Search Tree.js
@@ -47,4 +47,40 @@ var lowestCommonAncestor = function(root, p, q) {
 };
 
 
+Success
+Details 
+Runtime: 88 ms, faster than 92.51% of JavaScript online submissions for Lowest Common Ancestor of a Binary Search Tree.
+Memory Usage: 48.9 MB, less than 45.32% of JavaScript online submissions for Lowest Common Ancestor of a Binary Search Tree.
 
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    
+    node = root
+    
+    while(node){
+        
+    if (p.val < node.val && q.val < node.val){node = node.left}
+        
+    else if(p.val > node.val && q.val > node.val){node = node.right}
+        
+    else{return node}
+        
+    }
+    
+};
