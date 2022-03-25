db1527b95ca04010a9e9760fcb643bc877c9c911
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-03-25 08:35:27 -0400

    Update 938. Range Sum of BST.js

--- 938. Range Sum of BST.js
+++ 938. Range Sum of BST.js
@@ -53,3 +53,39 @@ var rangeSumBST = function(root, low, high) {
     if(high > root.val){ ans += rangeSumBST(root.right,low,high)}
     return ans
 };
+
+
+Success
+Details 
+Runtime: 188 ms, faster than 98.56% of JavaScript online submissions for Range Sum of BST.
+Memory Usage: 96.7 MB, less than 37.90% of JavaScript online submissions for Range Sum of BST.
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function(root, low, high) {
+    
+    let total = 0, stack = [root]
+    
+    while(stack.length>0){
+        let node = stack.pop()
+        
+        if(node != null){
+            
+            if(node.val >= low && node.val <=high){total += node.val}
+            if(node.left){stack.push(node.left)}
+            if(node.right){stack.push(node.right)}
+        }
+    }
+    return total
+};
