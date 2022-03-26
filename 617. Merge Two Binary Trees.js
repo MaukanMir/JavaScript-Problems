451c3d149716159d3ada8f08296e639a550e0758
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-03-26 12:28:19 -0400

    Update 617. Merge Two Binary Trees.js

--- 617. Merge Two Binary Trees.js
+++ 617. Merge Two Binary Trees.js
@@ -51,3 +51,49 @@ var mergeTrees = function(root1, root2) {
     root1.right = mergeTrees(root1.right,root2.right)
     return root1
 };
+
+
+Success
+Details 
+Runtime: 126 ms, faster than 67.55% of JavaScript online submissions for Merge Two Binary Trees.
+Memory Usage: 50.3 MB, less than 33.86% of JavaScript online submissions for Merge Two Binary Trees.
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
+var mergeTrees = function(root1, root2) {
+    
+    
+    if(root1 == null){return root2}
+    let stack = [[root1,root2]]
+    
+    while(stack.length >0){
+        let node = stack.pop()
+        
+        if(node[0] == null || node[1] == null){continue}
+        
+        node[0].val += node[1].val
+        if(node[0].left == null){
+            node[0].left = node[1].left
+        }
+        else{stack.push([node[0].left, node[1].left])}
+        
+        if(node[0].right == null){
+            node[0].right = node[1].right
+            
+        }else{stack.push([node[0].right,node[1].right])}
+    }
+    
+    return root1
+};
