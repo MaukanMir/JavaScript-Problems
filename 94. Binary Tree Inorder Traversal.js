75293a0dfdbb996cc34f48c94d796b932029b5d6
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-03-29 09:29:18 -0400

    Create 94. Binary Tree Inorder Traversal.js

---
+++ 94. Binary Tree Inorder Traversal.js
@@ -0,0 +1,52 @@
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Success
+Details 
+Runtime: 69 ms, faster than 69.76% of JavaScript online submissions for Binary Tree Inorder Traversal.
+Memory Usage: 42.4 MB, less than 30.11% of JavaScript online submissions for Binary Tree Inorder Traversal.
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    let stack = [], result = []
+    
+    while(stack.length>0 || root != null){
+        if(root){
+            stack.push(root)
+            root = root.left
+        }else{
+            root = stack.pop()
+            result.push(root.val)
+            root = root.right
+        }
+    }
+    return result
+};
